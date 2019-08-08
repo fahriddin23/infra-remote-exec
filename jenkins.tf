4f3a821b5425fd1916482f55198c3a92043b7a1c
fahriddin23 <fahriddin24@gmail.com>
2019-08-07 19:32:48 -0500

    done

--- jenkins.tf
+++ jenkins.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "jenkins" {
-    ami = "$var.ami"
+    ami = "${var.ami}"
     instance_type = "${var.instance_type}"
     key_name = "${aws_key_pair.developer_key.key_name}"
     associate_public_ip_address = true
@@ -15,7 +15,7 @@ resource "aws_instance" "jenkins" {
         }
 
         inline = [
-            "sudo yum install java-1.8.0-openjdk-devel",
+            "sudo yum install java-1.8.0-openjdk-devel -y",
             "curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo",
             "sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key",
             "sudo yum install jenkins",
